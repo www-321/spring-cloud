7e6ef3ba18680c7cdef21c5f8039efef72c5bec8
wuqn <wuqn891@126.com>
2020-09-24 11:24:37 +0800

    增加Test

---
+++ eureka-client/src/test/java/com/cloud/eurekaclient/EurekaClientApplicationTest.java
@@ -0,0 +1,20 @@
+package com.cloud.eurekaclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaClientApplicationTest {
+
+
+
+
+
+    @Test
+    public void te() {
+        System.out.println(123);
+    }
+}
