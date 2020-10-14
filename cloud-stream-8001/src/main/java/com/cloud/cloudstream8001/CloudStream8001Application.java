c5c0db5113e4747b0ab0073b67f7ad8d49f13f8e
wuqn <wuqn891@126.com>
2020-10-14 17:29:49 +0800

    stream

---
+++ cloud-stream-8001/src/main/java/com/cloud/cloudstream8001/CloudStream8001Application.java
@@ -0,0 +1,13 @@
+package com.cloud.cloudstream8001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStream8001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStream8001Application.class, args);
+    }
+
+}
