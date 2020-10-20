7d517c00f97c1aaaf0cdda5457d5970623e0729f
9MFDUAC0SNXCE99\Administrator <wuqn891@163.com>
2020-10-20 21:35:11 +0800

    zuul3

--- prod-client-03/src/main/java/com/cloud/prodclient03/controller/ProdController.java
+++ prod-client-03/src/main/java/com/cloud/prodclient03/controller/ProdController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import sun.security.x509.NameConstraintsExtension;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 @RestController
 public class ProdController {
     @Autowired
@@ -27,11 +32,26 @@ public class ProdController {
         return o;
     }
 
+    @GetMapping("admin")
+    public String admin() {
+
+        return "admin";
+    }
 
     @GetMapping("/prod3/del_user")
     public JSONObject delUser(String name, String age) {
         JSONObject o = new JSONObject();
         o.put("del", true);
         return o;
+    }
+
+
+    public static void main(String[] args) throws ParseException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date s = sdf.parse("2020-04-04");
+        Calendar instance = Calendar.getInstance();
+        instance.setTime(s);
+        instance.add(Calendar.DAY_OF_YEAR, 200);
+        System.out.println(instance.getTime());
     }
 }
---
+++ zuul-5001/src/main/java/com/cloud/zuul5001/filter/CustomFilter.java
@@ -0,0 +1,35 @@
+package com.cloud.zuul5001.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+//@Component
+public class CustomFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        //不进行路由
+        requestContext.setSendZuulResponse(false);
+        return null;
+    }
+}
